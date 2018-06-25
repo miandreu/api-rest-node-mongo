7f84cdf7b346b9c855a7e47417030c657925cd69
miandreu <miquelangelandreu@gmail.com>
2018-06-25 08:50:07 +0200

    :unlock: Disable auth function

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,22 @@ const userCtrl = require('../controllers/user')
 const auth = require('../middlewares/auth')
 const api = express.Router()
 
-api.get('/client', ClientCtrl.getClients)
+/*api.get('/client', ClientCtrl.getClients)
 api.get('/client/:clientId', ClientCtrl.getClient)
 api.post('/client', auth, ClientCtrl.saveClient)
 api.put('/client/:clientId', auth, ClientCtrl.updateClient)
 api.delete('/client/:clientId', auth, ClientCtrl.deleteClient)
+api.post('/signup', userCtrl.signUp)
+api.post('/signin', userCtrl.signIn)
+api.get('/private', auth, (req, res) => {
+  res.status(200).send({ message: 'You have access' })
+})*/
+
+api.get('/client', ClientCtrl.getClients)
+api.get('/client/:clientId', ClientCtrl.getClient)
+api.post('/client', ClientCtrl.saveClient)
+api.put('/client/:clientId', ClientCtrl.updateClient)
+api.delete('/client/:clientId', ClientCtrl.deleteClient)
 api.post('/signup', userCtrl.signUp)
 api.post('/signin', userCtrl.signIn)
 api.get('/private', auth, (req, res) => {
