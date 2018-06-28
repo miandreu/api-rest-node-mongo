6d0501233310cb00334c739602d0134d82662f14
Miquel Angel Andreu <31543113+miandreu@users.noreply.github.com>
2018-06-28 20:33:32 +0200

    :wrench: Change api port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8080,
   db: process.env.MONGODB_URI || 'mongodb://userL4A:32O3BkvUsPCGA10n@mongodb/clients',
   SECRET_TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
 }
